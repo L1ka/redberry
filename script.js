9ccde3a190e3c75a5f6ecaafbffe0feac62a78fe
L1ka <100157047+L1ka@users.noreply.github.com>
2022-07-10 21:40:25 +0400

    personal info validation js
    for validation created main async function validation();
    in this function created helper functions for error rendering:
    1) markup for pop-up
    2) for adding red style
    3) for removing err style
    4) for checking if pop-up not to render twice

--- script.js
+++ script.js
@@ -7,6 +7,50 @@ const secondPageContainer = document.querySelector(".page2");
 
 // listens to the click on first page's get started button, clears existing content and renders next page
 btnStart.addEventListener("click", () => {
-  firstPageContainer.style.display = "none";
+  firstPageContainer.classList.add("hidden");
   secondPageContainer.classList.remove("hidden");
 });
+
+// PERSONAL INFO INPUT ELEMENTS
+const inputOne = document.getElementById("name");
+const inputTwo = document.getElementById("email");
+const inputThree = document.getElementById("tel");
+const inputFour = document.getElementById("date");
+
+// VALIDATION ASYNC FUNCTION FOR PERSONAL INFO
+const validation = async function () {
+  //ELEMENTS
+  const container = document.querySelector(".two_part_title");
+  const persInfoForm = document.querySelector(".form_personal_info");
+  // FUNCTIONS
+
+  // MARKUP FOR ERROR BOX
+  const renderErrBox = function (type, message) {
+    const markup = `
+      <div class="error">
+        <img class="mark" src="/imgs/exclamation mark.svg" alt="" />
+        <span class="err_type">${type}</span>
+        <img class="close" src="/imgs/close.svg" alt="" />
+        <p class="err_message">${message}</p>
+      </div>
+    `;
+    container.insertAdjacentHTML("afterBegin", markup);
+  };
+
+  // ADD ERROR STYLE ON TEXT
+  const renderInputError = function (input) {
+    input.classList.add("err");
+  };
+
+  // REMOVE ERROR STYLE ON TEXT
+  const removeInputErr = function (input) {
+    input.classList.remove("err");
+  };
+
+  // 2) CHECKS IF ERROR BOX WAS ALREADY RENDERED AND REMOVES IT
+  const errBoxCheck = function () {
+    const errBox = document.querySelector(".error");
+    if (errBox) errBox.classList.add("hidden");
+  };
+};
+validation();
